6c6c4710e503a49c99192ba7a17fe7a280ba8d6d
Aaron Döbel <doebel@students.uni-marburg.de>
2023-02-01 17:00:48 +0100

    abgabe py

--- src/abgabe.py
+++ src/abgabe.py
@@ -1,7 +1,8 @@
 import os
 import pandas as pd
+import numpy as np
 
-path_input_data = os.path.join('.', 'train_combined_Class.csv')
+path_input_data = os.path.join('.', 'test_combined_Class.csv')
 # If we trained our model using the Order taxonomic level. If not, it can be for example test_combined_Species.csv
 df_data = pd.read_csv(path_input_data, delimiter=',')
 df_data.drop(df_data.columns[0], axis=1, inplace=True)
@@ -10,6 +11,33 @@ df_data.drop(df_data.columns[0], axis=1, inplace=True)
 ##########################
 # You do the preprocessing here
 ##########################
+def stdNormalize(df):
+    for col in df.columns[0:-1]:
+        colMean = df[col].mean()
+        colSTD = np.std(df[col])
+        for i in range(len(df[col])):
+            df[col][i] = (df[col][i] - colMean) / colSTD
+    return df
+
+
+df_data = df_data[['Bacteria;Bacteroidota;Bacteroidia',
+                   'Bacteria;Bdellovibrionota;Bdellovibrionia',
+                   'Bacteria;Campilobacterota;Campylobacteria',
+                   'Bacteria;Dependentiae;Babeliae',
+                   'Bacteria;Elusimicrobiota;Elusimicrobia',
+                   'Bacteria;Fibrobacterota;Fibrobacteria',
+                   'Bacteria;Firmicutes;uncultured',
+                   'Bacteria;Fusobacteriota;Fusobacteriia',
+                   'Bacteria;Myxococcota;Polyangia', 'Bacteria;Myxococcota;bacteriap25',
+                   'Bacteria;Patescibacteria;Saccharimonadia',
+                   'Bacteria;Spirochaetota;Brachyspirae',
+                   'Bacteria;Acidobacteriota;Subgroup 19',
+                   'Bacteria;Acidobacteriota;Vicinamibacteria',
+                   'Bacteria;Chloroflexi;Chloroflexia',
+                   'Bacteria;SAR324 clade(Marine group B);uncultured delta proteobacterium',
+                   'label']]
+
+df_data = stdNormalize(df_data)
 
 path_output_data = path_input_data
 df_data.to_csv(path_output_data)
